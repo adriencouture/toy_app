d1b9757c8d1a554d65ddaa5d1eb82c37c14d8718
Adrien Couture <coutureadrien@gmail.com>
2016-01-30 23:10:22 -0500

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-root 'application#hello'
+
+  root 'users#index'
+
+  resources :users
+  resources :microposts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
